c5baa2f3f8e9afaed3bc5ccda05b28d49b634fe2
aa <marakuyaspb@gmail.com>
2025-03-05 19:09:01 +0300

    add react webface & wallet connect btn

---
+++ dapp/src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import React, { useEffect, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import WalletConnect from "./WalletConnect/WalletConnect.js";
+import Footer from "./Footer/Footer.js";
+
+
+function App() {
+
+  const interBubbleRef = useRef(null);
+
+  useEffect(() => {
+    const interBubble = interBubbleRef.current;
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      interBubble.style.transform = 'translate(' + Math.round(curX) + 'px, ' + Math.round(curY) + 'px)';
+      requestAnimationFrame(move);
+    }
+
+    window.addEventListener('mousemove', function(event) {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    });
+
+    move();
+
+    return () => {
+      window.removeEventListener('mousemove', () => {});
+    };
+  }, []);
+
+
+  return (
+    <div className="App">
+
+     
+
+
+      <div className="gradient-bg">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div className="interactive" ref={interBubbleRef}></div>
+        </div>
+      </div>
+
+
+      <WalletConnect />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
